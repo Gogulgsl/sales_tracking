78428a71d2185c0db07a46b45295b0e80076b0da
Gogul Baskaran <gogulbaskaran01@gmail.com>
2024-11-19 12:03:00 +0530

    Change

--- app/controllers/api/opportunities_controller.rb
+++ app/controllers/api/opportunities_controller.rb
@@ -14,20 +14,18 @@ class Api::OpportunitiesController < ApplicationController
   end
 
   def my_opportunities
-    # Fetch the sales_team record associated with the current_user
     sales_team = SalesTeam.find_by(sales_user_id: current_user.id)
-    puts "============ Id: #{sales_team.inspect}=============="
 
     if sales_team.present?
-      # Retrieve opportunities associated with the sales_user_id
       opportunities = Opportunity.includes(:product, :school)
-                                 .find_by(sales_user_id: sales_team.sales_user_id)
+                                 .where(sales_user_id: sales_team.sales_user_id)
 
       render json: opportunities.as_json(include: [:product, :school]), status: :ok
     else
       render json: { error: 'No opportunities found for the user' }, status: :not_found
     end
   end
+
 
   # POST /api/opportunities
   def create
