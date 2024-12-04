39a04e7041f7bba24d6399903916c43c2dfd86e8
Gogul Baskaran <gogulbaskaran01@gmail.com>
2024-12-05 01:00:58 +0530

    stage controller

--- app/controllers/api/stages_controller.rb
+++ app/controllers/api/stages_controller.rb
@@ -1,58 +1,61 @@
-class StagesController < ApplicationController
-  before_action :set_stage, only: [:show, :edit, :update, :destroy]
+module Api
+  class StagesController < ApplicationController
+    before_action :set_stage, only: [:show, :edit, :update, :destroy]
 
-  # GET /stages
-  def index
-    @stages = Stage.all
-  end
-
-  # GET /stages/:id
-  def show
-  end
-
-  # GET /stages/new
-  def new
-    @stage = Stage.new
-  end
-
-  # POST /stages
-  def create
-    @stage = Stage.new(stage_params)
-    if @stage.save
-      redirect_to @stage, notice: 'Stage was successfully created.'
-    else
-      render :new, status: :unprocessable_entity
+    # GET /stages
+    def index
+      @stages = Stage.all
+      render json: @stages
     end
-  end
 
-  # GET /stages/:id/edit
-  def edit
-  end
-
-  # PATCH/PUT /stages/:id
-  def update
-    if @stage.update(stage_params)
-      redirect_to @stage, notice: 'Stage was successfully updated.'
-    else
-      render :edit, status: :unprocessable_entity
+    # GET /stages/:id
+    def show
     end
-  end
 
-  # DELETE /stages/:id
-  def destroy
-    @stage.destroy
-    redirect_to stages_url, notice: 'Stage was successfully destroyed.'
-  end
+    # GET /stages/new
+    def new
+      @stage = Stage.new
+    end
 
-  private
+    # POST /stages
+    def create
+      @stage = Stage.new(stage_params)
+      if @stage.save
+        redirect_to @stage, notice: 'Stage was successfully created.'
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_stage
-    @stage = Stage.find(params[:id])
-  end
+    # GET /stages/:id/edit
+    def edit
+    end
 
-  # Only allow a list of trusted parameters through.
-  def stage_params
-    params.require(:stage).permit(:stage_code, :description)
+    # PATCH/PUT /stages/:id
+    def update
+      if @stage.update(stage_params)
+        redirect_to @stage, notice: 'Stage was successfully updated.'
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    # DELETE /stages/:id
+    def destroy
+      @stage.destroy
+      redirect_to stages_url, notice: 'Stage was successfully destroyed.'
+    end
+
+    private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_stage
+      @stage = Stage.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def stage_params
+      params.require(:stage).permit(:stage_code, :description)
+    end
   end
 end
