dec858bcb3ab6307a6f06d39ceacaa4ab92f8daa
Gogul Baskaran <gogulbaskaran01@gmail.com>
2024-12-20 15:31:25 +0530

    User not active

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -1,13 +1,17 @@
-# app/controllers/api/auth_controller.rb
 class Api::AuthController < ApplicationController
   require 'jwt'
 
   def login
     user = User.find_by(username: params[:username])
-    if user&.authenticate(params[:password])
+
+    if user.nil?
+      render json: { error: 'Invalid username or password' }, status: :unauthorized
+    elsif !user.is_active
+      render json: { error: 'Current user is not active' }, status: :unauthorized
+    elsif user.authenticate(params[:password])
       token = encode_token({ user_id: user.id })
 
-      render json: { token: token, role: user.role, message: 'Login successful' }, status: :ok
+      render json: { token: token, role: user.role, username: user.username, message: "Login successful #{user.username}" }, status: :ok
     else
       render json: { error: 'Invalid username or password' }, status: :unauthorized
     end
